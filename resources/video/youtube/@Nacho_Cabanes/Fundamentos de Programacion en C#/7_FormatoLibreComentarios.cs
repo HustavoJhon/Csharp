65fdebe4041369c5bbbc9a6d8e9c0582489ccf9c
hustavojhon <hustavojhon@gmail.com>
2025-01-09 15:00:00 -0500

    chore: add format to comments /video/@nacho_cabanes #7

--- resources/video/youtube/@Nacho_Cabanes/Fundamentos de Programacion en C#/7_FormatoLibreComentarios.cs
+++ resources/video/youtube/@Nacho_Cabanes/Fundamentos de Programacion en C#/7_FormatoLibreComentarios.cs
@@ -1,4 +1,4 @@
-// 007 - FORMATO LIBRE, COMENTARIOS
+//! 007 - FORMATO LIBRE, COMENTARIOS
 
 // csharp nos permite tener todo el codigo sin identacion aunque son malas practicas como tener todo en una sola linea pero es perfectamente valido ya que su funcionamiento va ser el mismo
 // Pero la idea es siempre tenerlo ordenado para que sea facil leer como con las lineas en blanco separando en bloques las variables - entradas de datos - salidas de datos 
@@ -12,13 +12,14 @@ Programa para saludar
 Por Gustavo Jhon
 */
 
+//TODO: EJERCICIO PROPUESTO
+//? Haz un programa que pida al usuario una cantidad de metros y la convierta a millas (1 milla terrestre = 1909,344 metros). debe incluir dos comentarios de una linea al princiio del programa: una que recuerde que hace el programa y otro con tu nombre.
 
-// EJERCICIO PROPUESTO
-// HAZ UN PROGRAMA QUE PIDA AL USUARIO UNA CANTIDAD DE METROS Y LA CONVIERTA A MILLAS (1 MILLA TERRESTRE = 1609,344 METROS). DEBE INCLUIR DOS COMENTARIOS DE UNA LINEA AL PRINCIPIO DEL PROGRAMA: UNO QUE RECUERDE QUE HACE EL PROGRAMA Y OTRO CON TU NOMBRE.
 /* 
 PROGRAMA PARA CONVERTIR DE MILLAS A METROS
 POR GUSTAVO JHON
 */
+
 int metros; 
 double millas = 1609.344;
 double resultado;
