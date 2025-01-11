7b77f2150bfd4279ec85fc105ae9bbc00962a2ff
hustavojhon <hustavojhon@gmail.com>
2025-01-11 15:00:00 -0500

    chore: add format to comments /video/@nacho_cabanes #9

--- resources/video/youtube/@Nacho_Cabanes/Fundamentos de Programacion en C#/9_CondicionesIfElse.cs
+++ resources/video/youtube/@Nacho_Cabanes/Fundamentos de Programacion en C#/9_CondicionesIfElse.cs
@@ -1,6 +1,6 @@
-// 009 - CONDICIONES CON IF + ELSE
+//! 009 - CONDICIONES CON IF, ELSE
 
-// las llaves no son impresindibles si solo tiene una orden como en este caso el else
+//? las llaves no son impresindibles si solo tiene una orden como en este caso el else
 
 int a, b;
 
@@ -21,11 +21,8 @@ else
 
 Console.WriteLine("hasta luego");
 
-/*
-EJERCICIO PROPUESTO
-
-Pide al usuario dos numeros enteros y muestra el mayor de ellos. Por ejemplo, si introduce 1000 y 6000, la respuestas sera "El mayor es 1000". Si introduce dos numeros iguales, como 1000 y 1000, la respuesta seguira siendo "El mayor es 1000"
-*/
+//TODO: EJERCICIO PROPUESTO 
+//* Pide al usuario dos numeros enteros y muestra el mayor de ellos. Por ejemplo, si introduce 1000 y 6000, la respuestas sera "El mayor es 1000". Si introduce dos numeros iguales, como 1000 y 1000, la respuesta seguira siendo "El mayor es 1000"
 
 int x, y;
 
